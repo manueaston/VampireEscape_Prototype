1fc98da58d577c74eca11cbccb6cc814d6632691
ManuE <manueaston@gmail.com>
2022-10-14 16:53:36 +1300

    Set Grid layers to visible inside mask on start

--- VampireEscape_Prototype/Assets/ActivateFOWOnLayer.cs
+++ VampireEscape_Prototype/Assets/ActivateFOWOnLayer.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ActivateFOWOnLayer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        TilemapRenderer renderer = GetComponent<TilemapRenderer>();
+        renderer.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
     }
 
     // Update is called once per frame
