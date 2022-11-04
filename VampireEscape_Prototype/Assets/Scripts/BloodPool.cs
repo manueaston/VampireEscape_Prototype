30233d044c886067ed2614f98e6ab9c3a1f167ec
manueaston <101027142+manueaston@users.noreply.github.com>
2022-11-04 17:29:31 +1300

    Changed blood pool sprite

--- VampireEscape_Prototype/Assets/Scripts/BloodPool.cs
+++ VampireEscape_Prototype/Assets/Scripts/BloodPool.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class BloodPool : MonoBehaviour
 {
     public int movesAdded;
+    private bool active = true;
+
+    //Sprite stuff
+    private SpriteRenderer spriteRenderer;
+    public Sprite rat;
+    public Sprite noRat;
+    //
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = rat;
+    }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && active)
         {
             Debug.Log("Player At Blood Pool");
             MoveManager.instance.DecreaseMoves(movesAdded);
-            Destroy(gameObject);
+            active = false;
+            spriteRenderer.sprite = noRat;
         }
     }
 }
