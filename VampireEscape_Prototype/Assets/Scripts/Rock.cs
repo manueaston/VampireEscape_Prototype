e9da53a5b9a04bbb245057bf9de72d5ba6400d6e
manueaston <101027142+manueaston@users.noreply.github.com>
2022-11-04 16:22:04 +1300

    Rock break

--- VampireEscape_Prototype/Assets/Scripts/Rock.cs
+++ VampireEscape_Prototype/Assets/Scripts/Rock.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class Rock : MonoBehaviour
 {
-    public bool IsDoorOpen;
+    //Sprite stuff
+    private SpriteRenderer spriteRenderer;
+    public Sprite unbroken;
+    public Sprite broken;
+    //
+
+    private BoxCollider2D collider;
+    private bool IsActive = true;
 
     public void Start()
     {
-        gameObject.SetActive(true);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<BoxCollider2D>();
+        spriteRenderer.sprite = unbroken;
+        gameObject.layer = 6; // Layer StopMovement
     }
     public void OpenDoor()
     {
-        if (!IsDoorOpen)
+        if (IsActive)
         {
-            IsDoorOpen = true;
-            gameObject.SetActive(false);
-
+            IsActive = false;
+            collider.enabled = false;
+            spriteRenderer.sprite = broken;
+            gameObject.layer = 0; // Layer Default
         }
-    }
-    public void Reset()
-    {
-        IsDoorOpen = false;
-        gameObject.SetActive(true);
     }
 }
