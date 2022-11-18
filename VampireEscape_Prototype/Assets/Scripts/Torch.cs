587d6155b99cc7e2ac1c44dd6c54983635c47e2e
lukeypookie111 <lukevanzuilem@gmail.com>
2022-11-18 13:48:31 +1300

    torch docamentation

--- VampireEscape_Prototype/Assets/Scripts/Torch.cs
+++ VampireEscape_Prototype/Assets/Scripts/Torch.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 
 public class Torch : MonoBehaviour
 {
+    //added extra sprite and change to when deactivated
+
     public float minLightIntensity;
     public float maxLightIntensity;
     public float secondsBetweenFlickers;
